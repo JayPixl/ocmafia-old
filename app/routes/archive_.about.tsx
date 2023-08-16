db62f23e1e43815cd5fa44e9171a8749d686c438
Joshua Lawrence, Jr <jaypixl@protonmail.com>
2023-08-16 13:27:04 -0400

    Bit of work on the about page

--- app/routes/archive_.about.tsx
+++ app/routes/archive_.about.tsx
@@ -20,6 +20,16 @@ export default function About() {
             <div className="flex justify-center items-center">
                 <div className="bg-licorice-600 md:w-2/3 lg:p-12 m-5 rounded-lg w-full p-8 flex flex-col items-center">
                     <div className="text-4xl font-bold my-8">About</div>
+                    <div>
+                        <div className="italic">
+                            &nbsp; &nbsp; &nbsp; Greetings, traveller. It seems that you have entered my domain.
+                            Who am I, you ask? I am an entity known only as the Grandmaster,
+                            but you may refer to me as G or GM. Do you have what it takes to win my Game?
+                            We&apos;ll have to wait and see now, won&apos;t we?
+                            <br />
+                            <div className="text-right pr-8">-the Grandmaster</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </Layout>
